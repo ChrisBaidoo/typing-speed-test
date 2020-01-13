0a7c905ff5b133d644ee6cca0c1dae91e2e0d3f0
ChrisBaidoo <christianbaidoo@eroam-142-17.wifi.wmin.ac.uk>
2020-01-13 14:33:47 +0000

    added a timer

--- script.js
+++ script.js
@@ -6,7 +6,32 @@ const theTimer = document.querySelector(".timer");
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
+function leadingZero(time) {
+  if (time <= 9) {
+    time = "0" + time;
+  }
+  return time;
+}
+
+var timer = [0, 0, 0, 0];
+
 // Run a standard minute/second/hundredths timer:
+function runTimer() {
+  let currentTime =
+    leadingZero(timer[0]) +
+    ":" +
+    leadingZero(timer[1]) +
+    ":" +
+    leadingZero(timer[2]);
+  theTimer.innerHTML = currentTime;
+  timer[3]++;
+
+  timer[0] = Math.floor(leadingZero(timer[3]) / 100 / 60);
+  timer[1] = Math.floor(leadingZero(timer[3]) / 100 - timer[0] * 60);
+  timer[2] = Math.floor(
+    leadingZero(timer[3]) - timer[1] * 100 - timer[0] * 6000
+  );
+}
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
@@ -17,11 +42,17 @@ function spellCheck() {
 // Start the timer:
 function start() {
   let textEnteredLength = textArea.value.length;
-  console.log(textEnteredLength);
+  if (textEnteredLength === 0) {
+    setInterval(runTimer, 10);
+  }
 }
 
 // Reset everything:
+function reset() {
+  console.log("Reset button has been clicked");
+}
 
 // Event listeners for keyboard input and the reset button:
 textArea.addEventListener("keypress", start, false);
 textArea.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click", reset, false);
